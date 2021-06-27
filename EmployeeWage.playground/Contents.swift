584accd5943d2e149bfb3d87e390025ffedb2c0f
shrutiP1 <shrutipatil13798@gmail.com>
2021-06-27 14:09:54 +0530

    Calculate Part time and full time for Employee Wage

--- EmployeeWage.playground/Contents.swift
+++ EmployeeWage.playground/Contents.swift
@@ -1,16 +1,22 @@
 import UIKit
-let IS_PRESENT = 1;
-var check = Int.random(in: 0...1)
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
+var empHr = 0;
 var employeeWage:Int = 0
-var Hrs:Int = 8;
-let WAGE_PER_HR=20;
-if check == IS_PRESENT
+let WAGE_PER_HR = 20;
+let check=Int.random(in: 0...2)
+if(check == IS_FULL_TIME)
 {
-    employeeWage = WAGE_PER_HR * Hrs
-    print("Daily Employee Wage is \(employeeWage)")
+    empHr = 8;
+}
+else if check == IS_PART_TIME
+{
+    empHr = 4;
 }
 else
 {
     print("Employee is Absent")
 }
+employeeWage = WAGE_PER_HR * empHr;
+print("Employee Wage \(employeeWage)")
 
