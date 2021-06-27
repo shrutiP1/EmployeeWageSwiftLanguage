bd6d2fbc82b2b43a47c795531f004b7203b4960e
shrutiP1 <shrutipatil13798@gmail.com>
2021-06-27 14:22:43 +0530

    Calculate Total Wage for 20 Days

--- EmployeeWage.playground/Contents.swift
+++ EmployeeWage.playground/Contents.swift
@@ -4,15 +4,25 @@ let IS_FULL_TIME = 2;
 var empHr = 0;
 var employeeWage:Int = 0
 let WAGE_PER_HR = 20;
-let check = Int.random(in: 0...2)
-switch( check )
+var check:Int = 0 ;
+var totalEmployeeWage:Int = 0;
+for _ in 0...20
 {
-case IS_FULL_TIME : empHr = 8;
-case IS_PART_TIME : empHr = 4;
-default : empHr = 0;
-          print("Employee is Absent")
+    check = Int.random(in: 0...2)
+    if(check == IS_FULL_TIME)
+    {
+        empHr = 8;
+    }
+    else if check == IS_PART_TIME
+    {
+        empHr = 4;
+    }
+    else
+    {
+        empHr = 0;
+    }
+    employeeWage = WAGE_PER_HR * empHr;
+    totalEmployeeWage = totalEmployeeWage + employeeWage;
 }
-employeeWage = WAGE_PER_HR * empHr;
-print("Employee Wage \(employeeWage)")
-
+print("Total Employee Wage for 20 days \(totalEmployeeWage)")
 
