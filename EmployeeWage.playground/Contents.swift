6c9209517f4cf4dd8e4b85daedd612762d759261
shrutiP1 <shrutipatil13798@gmail.com>
2021-06-27 11:45:19 +0530

    Calculate Employee Wage

--- EmployeeWage.playground/Contents.swift
+++ EmployeeWage.playground/Contents.swift
@@ -1,11 +1,16 @@
 import UIKit
 let IS_PRESENT = 1;
 var check = Int.random(in: 0...1)
-if(check == IS_PRESENT)
+var employeeWage:Int = 0
+var Hrs:Int = 8;
+let WAGE_PER_HR=20;
+if check == IS_PRESENT
 {
-    print("Employee is Present")
+    employeeWage = WAGE_PER_HR * Hrs
+    print("Daily Employee Wage is \(employeeWage)")
 }
 else
 {
     print("Employee is Absent")
 }
+
