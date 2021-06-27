3c26eafe9a64d9dea323460ab11110c0b18bc575
shrutiP1 <shrutipatil13798@gmail.com>
2021-06-27 14:15:35 +0530

    Calculate Employee Wage Using Switch Case

--- EmployeeWage.playground/Contents.swift
+++ EmployeeWage.playground/Contents.swift
@@ -4,19 +4,15 @@ let IS_FULL_TIME = 2;
 var empHr = 0;
 var employeeWage:Int = 0
 let WAGE_PER_HR = 20;
-let check=Int.random(in: 0...2)
-if(check == IS_FULL_TIME)
+let check = Int.random(in: 0...2)
+switch( check )
 {
-    empHr = 8;
-}
-else if check == IS_PART_TIME
-{
-    empHr = 4;
-}
-else
-{
-    print("Employee is Absent")
+case IS_FULL_TIME : empHr = 8;
+case IS_PART_TIME : empHr = 4;
+default : empHr = 0;
+          print("Employee is Absent")
 }
 employeeWage = WAGE_PER_HR * empHr;
 print("Employee Wage \(employeeWage)")
+
 
